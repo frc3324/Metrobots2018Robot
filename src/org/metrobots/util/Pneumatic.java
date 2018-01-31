7c0808ff3022c108ff4b437efb1f871c4ec5643d
samf547 <fisher.1@themetroschool.org>
2018-01-31 16:27:02 -0500

    Updated pneumatic to use Xbox over MetroController, as on Peter

--- src/org/metrobots/util/Pneumatic.java
+++ src/org/metrobots/util/Pneumatic.java
@@ -1,19 +1,19 @@
 package org.metrobots.util;
 
 import edu.wpi.first.wpilibj.Solenoid;
-import org.metrobots.util.MetroController;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class Pneumatic {
-	private MetroController gamepad; // Setup the controller
+	private XboxController gamepad; // Setup the controller
 	private Solenoid GearShifter = new Solenoid(1); //Make GearShifter a Solenoid
 
-	public Pneumatic(MetroController gp) { // Setup constructor 
+	public Pneumatic(XboxController gp) { // Setup constructor 
 		gamepad = gp; 
 	}
 		
 	public void startSolenoid() { //Make method to startSolenoid
 		try {
-			if (gamepad.getAPressed()) { // (Line 18-20) If A is pressed, (change gear (to whatever it's not right now))
+			if (gamepad.getAButtonPressed()) { // (Line 18-20) If A is pressed, (change gear (to whatever it's not right now))
 				boolean currentShifterStatus = GearShifter.get(); // get current gear/solenoid status
 				GearShifter.set(!currentShifterStatus); // Change gear to whatever it's not right now 
 			}
